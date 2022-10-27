6c150052255962496470333428eb36233de3cadf
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-10-27 13:26:59 +0100

    completed operator function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//functions that perform calculations using the two numbers that are passed into them
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -13,3 +14,22 @@ function multiply(num1, num2) {
 function divide(num1,num2) {
     return num1 / num2;
 };
+
+function operate(operator, num1, num2) {
+    let result = 0;
+    //switch statement to execute different functions depending on which operator is selected
+    switch (operator) {
+        case ("+"):
+            result = add(num1, num2);
+            return result;
+        case("-"):
+            result = subtract(num1,num2);
+            return result;
+        case("*"):
+            result = multiply(num1, num2);
+            return result;
+        case("/"):
+            result = divide(num1,num2);
+            return result;
+    }
+}
